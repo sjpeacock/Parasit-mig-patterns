53a620faaf3e898a0703f4d4deab5743270b5f29
Stephanie J Peacock <stephanie.j.peacock@gmail.com>
2018-12-30 21:52:41 -0700

    Added escape_analytical.R based on Mark's calculations of the boundary between spread/escape.

--- base_params_per_day.R
+++ base_params_per_day.R
@@ -1,19 +1,20 @@
 #-------------------------------------------------------------------------------
 # Parameters
 #-------------------------------------------------------------------------------
+# I commented out the ones that don't matter ( = 0) for the escape simulations.
 
 base.params=c(
-	beta=0, 		# Host birth rate
-	mu=0,	 		# Natural host death rate (other base_params.R = 0)
-	alpha=0, 	 	# Per-parasite rate of parasite-induced host death
-	rho=0,			# Within-host parasite reproduction
+#	beta=0, 		# Host birth rate
+#	mu=0,	 		# Natural host death rate (other base_params.R = 0)
+#	alpha=0, 	 	# Per-parasite rate of parasite-induced host death
+#	rho=0,			# Within-host parasite reproduction
 	sigma=0.01,		# Parasite death rate
 	kappa=0.03, 	# Production of parasite larvae (1)
 	lambda=0.0005,	# Successful transmission probability (0.01)
 	mu.L=0.015, 	# Larval parasite death rate
-	omega=0, 		# Starting rate for stationary hosts
-	gamma=0, 		# Stopping rate for migrating, parasite-free hosts
-	theta=0, 		# Per-parasite increase in stopping rate (0.1)
+#	omega=0, 		# Starting rate for stationary hosts
+#	gamma=0, 		# Stopping rate for migrating, parasite-free hosts
+#	theta=0, 		# Per-parasite increase in stopping rate (0.1)
 	c.=25		# Migration speed (km per year) # 20000 km/yr*1yr/365 days = 55 km/day
 	)
 
